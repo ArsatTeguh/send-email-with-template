0c54ac332fa9b6ebed2e2f55ed611583f12ee527
arsyatteguh <arsyatteguh@gmail.com>
2023-12-26 03:12:17 +0700

    image url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'i.ibb.co',
+            port: '',
+            pathname: '/rFdJP73',
+        },
+    ],
+},
 };
   
